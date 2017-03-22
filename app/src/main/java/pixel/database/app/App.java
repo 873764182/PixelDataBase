3b0d14ab95caaa45fc62ed6f3f4fb9bd82b1b7de
panxiong <873764182@qq.com>
2017-03-22 10:17:34 +0800

    写上注释

--- app/src/main/java/pixel/database/app/App.java
+++ app/src/main/java/pixel/database/app/App.java
@@ -23,6 +23,12 @@ public class App extends Application {
             @Override
             public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion, Class<?>[] tables) {
                 Log.e(App.class.getSimpleName(), "数据库版本 -> " + oldVersion + "\t" + newVersion);
+                if (oldVersion < newVersion) {
+                    // 删除目前所有的表
+                    PixelDao.deleteTable(tables);
+                    // 重新生成数据库表
+                    PixelDao.createTable(tables);
+                }
             }
         }, UserTable.class);   // 初始化数据库与创建数据库表
     }
